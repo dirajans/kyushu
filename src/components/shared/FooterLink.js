7651ce37327a80b47fba9217c81724855a729b76
alkyu92 <alkyu92@gmail.com>
2019-12-07 21:12:09 +0800

    updates for github pages

--- src/components/shared/FooterLink.js
+++ src/components/shared/FooterLink.js
@@ -4,13 +4,13 @@ import { Link, MenuItem } from '@material-ui/core';
 export default function FooterLink({ title, url, newTab }){
   if (newTab) {
     return (
-      <Link href={url} target={'_blank'} underline={'none'}>
+      <Link href={'#/'+url} target={'_blank'} underline={'none'}>
         <MenuItem style={styles.font}>{title}</MenuItem>
       </Link>
     )
   } else {
     return (
-      <Link href={url} underline={'none'}>
+      <Link href={'#/'+url} underline={'none'}>
         <MenuItem style={styles.font}>{title}</MenuItem>
       </Link>
     )
