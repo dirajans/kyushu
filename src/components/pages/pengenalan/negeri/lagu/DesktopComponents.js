380d2bbeece741228ff7080627205e0b05b9b166
alkyu92 <alkyu92@gmail.com>
2019-12-28 08:34:41 +0900

    updates

--- src/components/pages/pengenalan/negeri/lagu/DesktopComponents.js
+++ src/components/pages/pengenalan/negeri/lagu/DesktopComponents.js
@@ -1,32 +1,53 @@
 import React from 'react';
 import {
-  Container
+  Container, 
+  Typography,
+  Divider,
+  Card,
+  CardContent,
+  Grid,
 } from '@material-ui/core';
 
 export default function DesktopComponents(){
   return (
     <Container>
-      Berkatlah Yang DiPertuan Besar Negeri Sembilan is the state anthem of Negeri Sembilan. It was composed by Andrew Caldecott (1884–1951) with lyrics by Tunku Zakaria Mambang, and it was used by Tuanku Muhammad, who was the ruler at that time.
+      <br/><br/><br/><br/>
+      <Typography variant={'h4'}>
+      Berkatlah Yang Dipertuan Besar Negeri Sembilan
+      </Typography>
+      <br/>
+      <Divider />
+      <br/>
+      <Typography variant={'body1'}>
+      Berkatlah Yang Dipertuan Besar Negeri Sembilan adalah lagu kebesaran Negeri Sembilan. Lagunya telah digubah oleh 
+      Andrew Caldecott (1884-1951) dan diperkenankan oleh Yang Di-Pertuan Negeri Sembilan pada masa itu, Tuanku Muhammad pada 
+      tahun 1911.
+      <br/><br/>
+      Lagu negeri ini mendapat gubahan semula yang lebih diperbaharui oleh Tunku Zain Al-'Abidin ibni Tuanku Muhriz iaitu putera kedua Yamtuan Besar Tuanku Muhriz ibni Almarhum Tuanku Munawir.[1] dengan kerjasama Orkestra Filharmonik Malaysia serta kumpulan koir dari beberapa sekolah serata negeri tersebut.[1] Gubahan baru ini diperkenalkan sewaktu pertabalan baginda Tuanku Muhriz serta permaisuri baginda Tengku Aishah Rohani Tengku Besar Mahmud pada awal 2009, dan kemudiannya dirasmikan pada 27 Oktober 2010.[2]
+      <br/><br/>
+      </Typography>
 
-The anthem had went under a revitalization project by the efforts of the second son of the current Yang DiPertuan Besar, Tunku Zain Al-'Abidin ibni Tuanku Muhriz.[1] The rearranged composition, recorded with the Malaysian Philharmonic Orchestra and choir groups from several schools across the state,[1] was first presented to the public on the installation ceremonies of Tuanku Muhriz and his wife Tuanku Aishah Rohani in early 2009, and it was officially launched on 27 October 2010.[2]
-
-Lyrics
-Malay
-Berkatlah Yang Dipertuan Besar Negeri Sembilan
-Kurniai sihat dan makmur
-Kasihi rakyat lanjutkan umur
-Akan berkati sekalian yang setia
-Musuhnya habis binasa
-Berkatlah Yang Dipertuan Besar Negeri Sembilan.
-Rough English translation
-Blessed are you, great ruler of Negeri Sembilan
-May you be healthy and live long
-Love your citizens, live long
-Will readily pray for you
-All his enemies will be completely destroyed
-Blessed are you, great ruler of Negeri Sembilan.
-
-The line "Musuhnya habis binasa" is also adapted as a motto for the state's football team, Negeri Sembilan FA.
+      <Grid container>
+        <Grid item lg={3} />
+        <Grid item lg={6}>
+          <Card>
+            <CardContent>
+              <Typography variant={'h6'} align={'center'}>
+              <strong>Lirik</strong>
+              <br/><br/>
+              Berkatlah Yang Dipertuan Besar Negeri Sembilan<br/>
+              Kurnia sihat dan makmur<br/>
+              Kasihi rakyat lanjutkan umur<br/>
+              Akan berkati sekalian yang setia<br/>
+              Musuhnya habis binasa<br/>
+              Berkatlah Yang Dipertuan Besar Negeri Sembilan<br/>
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item lg={3} />
+      </Grid>
+      
     </Container>
   )
 }
