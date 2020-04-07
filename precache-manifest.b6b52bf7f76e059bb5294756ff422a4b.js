773084d2824be8492b952ca2dd05d43edbbe38aa
alkyu92 <al_kyu92@yahoo.com>
2020-04-07 17:27:11 +0800

    Updates

--- precache-manifest.b6b52bf7f76e059bb5294756ff422a4b.js
+++ precache-manifest.b6b52bf7f76e059bb5294756ff422a4b.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9d961d813c6a544a49df3266b3cc9360",
+    "revision": "a45a562379f0e52e6e841f0cdf9f6a24",
     "url": "/kyushu/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/kyushu/static/css/2.c43dd92d.chunk.css"
   },
   {
-    "revision": "874f89b09ffd5facd81a",
+    "revision": "913813f65c4049d24a2a",
     "url": "/kyushu/static/css/main.f35c36ad.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/kyushu/static/js/2.c07385ad.chunk.js"
   },
   {
-    "revision": "874f89b09ffd5facd81a",
-    "url": "/kyushu/static/js/main.f2dbc1a5.chunk.js"
+    "revision": "913813f65c4049d24a2a",
+    "url": "/kyushu/static/js/main.f10491b9.chunk.js"
   },
   {
     "revision": "8ae5add66ec106fe458f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kyushu/precache-manifest.ab69f13a16bc038e74a3aca19163549a.js"
+  "/kyushu/precache-manifest.b6b52bf7f76e059bb5294756ff422a4b.js"
 );
 
 self.addEventListener('message', (event) => {
